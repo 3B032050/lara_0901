997306f772c4efd407b7a3fbda4140775ad77b17
黃文輝 <antory040512@gmail.com>
2023-11-24 09:29:47 +0800

    加入資料驗證規則

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -28,6 +28,11 @@ class AdminPostsController extends Controller
 
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'title' => 'required|max:50',
+            'content' => 'required',
+            'is_feature' => 'required|boolean',
+        ]);
         Post::create($request->all());
         return redirect()->route('admin.posts.index');
     }
@@ -40,6 +45,11 @@ class AdminPostsController extends Controller
 
     public function update(Request $request, Post $post)
     {
+        $this->validate($request, [
+            'title' => 'required|max:50',
+            'content' => 'required',
+           'is_feature' => 'required|boolean',
+        ]);
         $post->update($request->all());
         return redirect()->route('admin.posts.index');
     }
