bdf99953f1a60f166a6e024a24b6fb8a75d7f1e4
黃文輝 <antory040512@gmail.com>
2023-11-24 09:36:59 +0800

    將呈現錯誤訊息的blade程式碼獨立成為共同區塊

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -9,20 +9,7 @@
         <li class="breadcrumb-item active">編輯文章</li>
     </ol>
     <div class="alert alert-danger alert-dismissible" role="alert" id="liveAlert">
-        @if (count($errors) > 0)
-            <!-- 表單錯誤清單 -->
-            <strong>哎呀！出了些問題！</strong>
-
-            <br><br>
-
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-
-        @endif
+        @include('admin.layouts.shared.errors')
     </div>
     <form action="{{route('admin.posts.update',$post->id)}}" method="post">
         @csrf
